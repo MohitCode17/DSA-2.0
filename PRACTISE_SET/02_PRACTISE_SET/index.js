561e097a40bd7488348adcf8c53189e17f579b27
MohitCode17 <mohit.codes17@gmail.com>
2025-07-09 09:16:33 +0530

    day04-of-dsa array-problems

---
+++ PRACTISE_SET/02_PRACTISE_SET/index.js
@@ -0,0 +1,209 @@
+/************* PRACTISE SET - 02 ************************************************/
+
+// Q.1 Calculate the sum of n numbers of an array.
+// Q.2 Find the maximum element of an array.
+// Q.3 Find the second maximum element of an array.
+// Q.4 Reverse an array, Eg:- [1,2,3] -> [3,2,1]
+// Q.5 Segregate 0s and 1s and moves all 0's to start of the array. / Segregate 0s and 1s and moves all 0's to end of the array.
+// Q.6 Find the min and max element in the array.
+// Q.7 Given an array, rotate the array by one position in clock-wise direction. E.g:- [1, 2, 3, 4, 5] -> [5, 1, 2, 3, 4]
+// Q.8 Rotate array by k element. E.g:- [1, 2, 3, 4, 5] k = 2 -> [3, 4, 5, 1, 2]
+
+// Ans-1
+function sumOfArrayElements(arr) {
+  if (arr.length === 0) return "Array cannot be a blank.";
+
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum;
+}
+
+// console.log(sumOfArrayElements([1, 2, 3, 4, 5]));
+// console.log(sumOfArrayElements([10, 20, 30, 40, 50]));
+// console.log(sumOfArrayElements([1, 9]));
+
+// Ans-2
+function findMax(arr) {
+  if (arr.length === 0) return "Array cannot be a blank.";
+
+  let maxElement = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+}
+
+// console.log(findMax([5, 1, 8, 3, 7]));
+// console.log(findMax([519, 10, 88, 33, 782]));
+
+// Ans-3
+function findSecondMax(arr) {
+  if (arr.length < 2) return "Array length should be more than 2";
+
+  let max = -Infinity;
+  let secondMax = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] > secondMax && arr[i] !== max) {
+      secondMax = arr[i];
+    }
+  }
+
+  return secondMax;
+}
+
+// console.log(findSecondMax([5, 1, 8, 3, 7]));
+// console.log(findSecondMax([519, 10, 88, 33, 782]));
+
+// Ans-4
+function reverseArray(arr) {
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+
+    start++;
+    end--;
+  }
+
+  return arr;
+}
+
+// console.log(reverseArray([5, 1, 8, 3, 7]));
+
+// Ans-5
+function moveZerosToStart(arr) {
+  let i = 0,
+    j = 0;
+
+  while (i < arr.length) {
+    if (arr[i] === 0) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+
+      i++;
+      j++;
+    } else {
+      i++;
+    }
+  }
+
+  return arr;
+}
+
+// console.log(moveZerosToStart([1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0]));
+
+function moveZerosToEnd(arr) {
+  let i = 0,
+    j = 0;
+
+  while (i < arr.length) {
+    if (arr[i] === 1) {
+      let temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+
+      i++;
+      j++;
+    } else {
+      i++;
+    }
+  }
+
+  return arr;
+}
+
+// console.log(moveZerosToEnd([1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0]));
+
+// Ans-6
+function getMinMax(arr) {
+  let min = arr[0];
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  return [min, max];
+}
+
+// console.log(getMinMax([3, 2, 1, 56, 10000, 167]));
+// console.log(getMinMax([10]));
+
+// Ans-7
+function rotateArrayClockWise(arr) {
+  let n = arr.length;
+  let copy = arr[n - 1];
+
+  for (let i = n - 2; i >= 0; i--) {
+    arr[i + 1] = arr[i];
+  }
+
+  arr[0] = copy;
+
+  return arr;
+}
+
+// console.log(rotateArrayClockWise([1, 2, 3, 4, 5]));
+
+// Ans-8
+function rotateArrayByK(arr, k) {
+  k = k % arr.length;
+
+  for (let i = 0; i < k; i++) {
+    let copy = arr[0];
+
+    for (let j = 0; j < arr.length - 1; j++) {
+      arr[j] = arr[j + 1];
+    }
+
+    arr[arr.length - 1] = copy;
+  }
+  return arr;
+}
+
+// console.log(rotateArrayByK([1, 2, 3, 4, 5], 2));
+// console.log(rotateArrayByK([1, 2, 3, 4, 5], 3));
+
+function rotateArrayByKOptimize(arr, k) {
+  // 1. Reverse whole array
+  // 2. Reverse first n - k elements
+  // 3. Reverse last k elements
+
+  let n = arr.length;
+  k = k % n; // Handle if k > n
+
+  function reverse(start, end) {
+    while (start < end) {
+      [arr[start], arr[end]] = [arr[end], arr[start]];
+      start++;
+      end--;
+    }
+  }
+
+  reverse(0, n - 1);
+  reverse(0, n - k);
+  reverse(n - k, n - 1);
+
+  return arr;
+}
+
+console.log(rotateArrayByK([1, 2, 3, 4, 5], 2));
+console.log(rotateArrayByK([1, 2, 3, 4, 5], 4));
